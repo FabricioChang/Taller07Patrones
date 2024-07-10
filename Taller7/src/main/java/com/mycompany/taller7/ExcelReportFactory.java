a87a84ffc01b73f0841e04189380734baaf13b33
StevenMiraba <150202191+StevenMiraba@users.noreply.github.com>
2024-07-10 15:37:07 -0500

    Add files via upload

---
+++ Taller7/src/main/java/com/mycompany/taller7/ExcelReportFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller7;
+
+/**
+ *
+ * @author CltControl
+ */
+public class ExcelReportFactory extends ReportFactory {
+    @Override
+    public ReportGenerator createReport() {
+        return new ExcelReport();
+    }
+}
---
+++ Taller7/src/main/java/com/mycompany/taller7/ReportDecorator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller7;
+
+/**
+ *
+ * @author CltControl
+ */
+public abstract class ReportDecorator implements ReportInterface {
+    protected Report decoratedReport;
+
+    public ReportDecorator(Report decoratedReport) {
+        this.decoratedReport = decoratedReport;
+    }
+
+    @Override
+    public void generate() {
+        decoratedReport.generate();
+    }
+}
+
